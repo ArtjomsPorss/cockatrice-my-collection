a33976030a6aab9f06047e6c9ae826cba25767f2
Artjoms Porss <Artjoms.Porss@version1.com>
2019-08-25 00:38:46 +0100

    added verification of cardnames from my collection. and printout if cardname is wrong

--- src/main/java/artporsh/cockatrice/mycollection/App.java
+++ src/main/java/artporsh/cockatrice/mycollection/App.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.xml.bind.JAXBContext;
@@ -16,59 +15,85 @@ import javax.xml.bind.Marshaller;
 import artporsh.cockatrice.mycollection.schema.CockatriceCarddatabase;
 import artporsh.cockatrice.mycollection.schema.CockatriceCarddatabase.Cards.Card;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	
+public class App {
+
 	long starttime;
 	
-    public static void main( String[] args ) throws JAXBException, IOException
-    {
-    	App app = new App();
-    	CockatriceCarddatabase db = app.unmarshall();
-    	System.out.println(String.format("card db loaded[%s]",db));
-    	
-    	List<String> myCards = app.readCardNamesFromFile();
-//    	db.getCards().getCard().parallelStream().filter(c -> myCards.contains(c.getName())).forEach(mc -> System.out.println("mycard::"+mc.getName()));
-    	List<Card> mycards = db.getCards().getCard().parallelStream().filter(c -> myCards.contains(c.getName())).collect(Collectors.toList());
-    	db.getCards().getCard().clear();
-    	db.getCards().getCard().addAll(mycards);
-    	
-    	app.marshal(db);
-    }
-    
+	CockatriceCarddatabase db;
+	List<String> myCards;
+
+	public static void main(String[] args) throws JAXBException, IOException {
+		App app = new App();
+		app.loadCockatriceDatabase();
+		app.readMyCardsFromFile();
+		app.logMyCardsWithIncorrectNames();
+		app.createCockatriceDatabaseOfMyCards();
+	}
+
+	private void loadCockatriceDatabase() throws JAXBException, IOException {
+		db = this.unmarshall();
+	}
 
 	public CockatriceCarddatabase unmarshall() throws JAXBException, IOException {
-    	this.startTime();
-        JAXBContext context = JAXBContext.newInstance(CockatriceCarddatabase.class);
-        CockatriceCarddatabase db =  (CockatriceCarddatabase) context.createUnmarshaller()
-        		.unmarshal(new FileReader("cards.xml"));
-        this.stopTime();
-        return db;
-    }
-	
-	private void marshal(CockatriceCarddatabase db) throws JAXBException {
-	    JAXBContext context = JAXBContext.newInstance(CockatriceCarddatabase.class);
-	    Marshaller mar= context.createMarshaller();
-	    mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-	    mar.marshal(db, new File("mycards.xml"));
+		this.startTime();
+		JAXBContext context = JAXBContext.newInstance(CockatriceCarddatabase.class);
+		CockatriceCarddatabase db = (CockatriceCarddatabase) context.createUnmarshaller()
+				.unmarshal(new FileReader("cards.xml"));
+		this.stopTime();
+		return db;
 	}
-    
-    public List<String> readCardNamesFromFile() throws IOException{
-    	System.out.println("My Cards ---------------------------------------------");
-    	System.out.println(Paths.get("my-cards.txt").toAbsolutePath());
-    	return Files.readAllLines(Paths.get("my-cards.txt"));
-    }
-    
-    public void startTime() {
-    	starttime = System.currentTimeMillis();
-    	System.out.println("Started Loading DB");
-    }
-    
-    public void stopTime() {
-    	System.out.println("Finished Loafing DB, time taken: " + ((System.currentTimeMillis() - starttime)/1000));
-    }
+
+	public void readMyCardsFromFile() throws IOException {
+		System.out.println("Loading my cards");
+//		System.out.println(Paths.get("my-cards.txt").toAbsolutePath());
+		myCards =  Files.readAllLines(Paths.get("my-cards.txt"));
+	}
+
+	private void logMyCardsWithIncorrectNames() {
+		List<String> dbCardsUppercase = db.getCards().getCard().stream().map(e -> e.getName().toUpperCase()).collect(Collectors.toList());
+		// cards from my list that are not present in Cockatrice DB - likely due to wrong spelling
+		List<String> incorrectNamedCards = myCards.stream().filter(c -> !dbCardsUppercase.contains(c.toUpperCase().trim())).collect(Collectors.toList());
+		
+		if(incorrectNamedCards.isEmpty()) {
+			System.out.println("All cards you specified are correct");
+		} else {
+			System.out.println("----------------------------------------------------");
+			System.out.println("You specified cards with wrong names. Make sure they are present in Cockatrice's cards.xml");
+			System.out.println();
+			incorrectNamedCards.forEach(System.out::println);
+			System.out.println();
+			System.out.println("----------------------------------------------------");
+		}
+		
+	}
+
+	private void createCockatriceDatabaseOfMyCards() throws JAXBException {
+		List<Card> myCardsFromDb = db.getCards().getCard().parallelStream().filter(c -> myCards.contains(c.getName()))
+				.collect(Collectors.toList());
+		
+		db.getCards().getCard().clear();
+		db.getCards().getCard().addAll(myCardsFromDb);
+		
+		this.marshal(db);
+		System.out.println("Collection mycards.xml is created");
+	}
+
+
+
+	private void marshal(CockatriceCarddatabase db) throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(CockatriceCarddatabase.class);
+		Marshaller mar = context.createMarshaller();
+		mar.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		mar.marshal(db, new File("mycards.xml"));
+	}
+
+
+	public void startTime() {
+		starttime = System.currentTimeMillis();
+		System.out.println("Started Loading DB");
+	}
+
+	public void stopTime() {
+		System.out.println("Finished Loafing DB, time taken: " + ((System.currentTimeMillis() - starttime) / 1000) + " seconds");
+	}
 }
